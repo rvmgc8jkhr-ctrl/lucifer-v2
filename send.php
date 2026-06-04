a344bbc1e608a5dd217809e3bc078d745c405c7b
rvmgc8jkhr-ctrl <rvmgc8jkhr@privaterelay.appleid.com>
2026-06-04 19:04:48 +0800

    新增預約按鈕

--- send.php
+++ send.php
@@ -8,14 +8,48 @@ $date = $_POST['date'];
 $time = $_POST['time'];
 $note = $_POST['note'];
 
-echo "
-姓名：$name <br>
-電話：$phone <br>
-LINE：$lineid <br>
-地區：$area <br>
-日期：$date <br>
-時間：$time <br>
-備註：$note <br>
+$message = "
+🔥 路西法新預約通知
+
+姓名：$name
+電話：$phone
+LINE：$lineid
+地區：$area
+日期：$date
+時間：$time
+備註：$note
 ";
+
+$accessToken = "oMAUq6nVwtZcRGgUtTh0etD4AC7iDo47Na5Fj0afy/Epnsw4LLnjXQSGTywOoV9n7rvRoNbuje3H3AvnySbdWg99jnqZTM1mR6Aqr0UuzVT6HCGjwBQemmNBQTG7aif7VvBBzL0/IwMb45FGmNlxtwdB04t89/1O/w1cDnyilFU=";
+
+$data = [
+    "to" => "U0f800f352142df585ebfab8808909cf2",
+    "messages" => [
+        [
+            "type" => "text",
+            "text" => $message
+        ]
+    ]
+];
+
+$options = [
+    "http" => [
+        "method"  => "POST",
+        "header"  =>
+            "Content-Type: application/json\r\n" .
+            "Authorization: Bearer " . $accessToken . "\r\n",
+        "content" => json_encode($data),
+    ]
+];
+
+$context = stream_context_create($options);
+
+file_get_contents(
+    "https://api.line.me/v2/bot/message/push",
+    false,
+    $context
+);
+
+echo "預約成功！";
 
 ?>
